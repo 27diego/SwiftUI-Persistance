9f886a6d14b4b7b38bb218ee1734a8f80aca23aa
Diego Vega <diego.vops@gmail.com>
2020-09-09 14:47:08 -0700

    working on persistance

--- TodoSwiftUI/ContentView.swift
+++ TodoSwiftUI/ContentView.swift
@@ -9,8 +9,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var items = [
+        Characters(phrase: "Find Mike"),
+        Characters(phrase: "Buy Eggos"),
+        Characters(phrase: "Destroy Demogorgon")
+    ]
+    
+    @State var clickedItems:Set<UUID> = []
+    @State var showAlert: Bool = false
+    @State var textField: String = ""
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            ZStack {
+                List(items){ item in
+                    Button(action: {
+                        if self.clickedItems.contains(item.id){
+                            self.clickedItems.remove(item.id)
+                        }
+                        else {
+                            self.clickedItems.insert(item.id)
+                        }
+                    }) {
+                        if self.clickedItems.contains(item.id) {
+                            HStack{
+                                Text(item.phrase)
+                                Spacer()
+                                Image(systemName: "checkmark")
+                                    .foregroundColor(.black)
+                            }
+                        }
+                        else{
+                            Text(item.phrase)
+                        }
+                    }
+                }
+                .navigationBarTitle("ToDo")
+                .navigationBarItems(trailing: Button(action: {
+                    self.showAlert = true
+                }, label: {
+                    Text("Add")
+                }))
+                
+                if self.showAlert == true {
+                    AlertControlView(textString: $textField, showAlert: $showAlert, items: $items, title: "Add Item", message: "")
+                }
+                
+            }
+        }
+    }
+    
+    func addItem(toAdd: String){
+        items.append(Characters(phrase: toAdd))
     }
 }
 
@@ -19,3 +70,10 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+struct Characters: Identifiable {
+    let id: UUID = UUID()
+    let phrase: String
+}
+
